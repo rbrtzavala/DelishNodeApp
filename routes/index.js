84cd1c1e807b6bd56b294edc1df6ecddde00dc75
RAZ <rbrtzavala@gmail.com>
2017-05-16 10:18:14 -0600

    Imported catchErros from errHandlers and passed storeController to it in the post route for /add. This makes use of function composition where a func is passed to another func to create a new func, in this case catches errs.

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,16 @@ const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController');
 
+// { x } = es 6 object destructuring:
+// allows to import a method from an entire object with same name as variable.
+const { catchErrors } = require('../handlers/errorHandlers');
+
 // Do work here
 router.get('/', storeController.homePage);
 router.get('/add', storeController.addStore);
-router.post('/add', storeController.createStore);
 
+// Using catchErrors() here fas Function Composition:
+// Combining more than one function to produce a new function.
+router.post('/add', catchErrors(storeController.createStore));
 
 module.exports = router;
